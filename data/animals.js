902fabf442bbc5c5659a11196acdf04e49a33306
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-14 07:26:17 -0700

    a change to make a pull request

--- data/animals.js
+++ data/animals.js
@@ -10,5 +10,20 @@ module.exports = [
   {
     name: 'spot',
     cool_factor: 10,
+  },
+  {
+    name: 'bessie',
+    cool_factor: 3,
+  },
+  {
+    name: 'jumpy',
+    cool_factor: 4,
+  },
+  {
+    name: 'spot',
+    cool_factor: 10,
   }
 ];
+
+
+
